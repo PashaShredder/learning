b41f495c0cafeaebc43e41b4904771dd60b375e8
PashaShredder <lafalarami@gmail.com>
2022-10-14 17:17:41 +0300

    new tasks

---
+++ pythonlessons/factorial.py
@@ -0,0 +1,21 @@
+'''Фасториал через рекурсию и цикл for'''
+# def recur_factorial(n):
+#     if n == 1:
+#         return n
+#     else:
+#         return n * recur_factorial(n - 1)
+#
+#
+# n = int(input("Enter the number: "))
+# result = recur_factorial(n)
+# print("Factorial of", n, "is", result)
+
+# def factorial(n):
+#     i = 1
+#     factorial = 1
+#     while i <= n:
+#         factorial*= i
+#         i+=1
+#     return factorial
+# print(factorial(7))
+#
--- pythonlessons/tasks.py
+++ pythonlessons/tasks.py
@@ -89,3 +89,81 @@
 #     print('без дублей')
 # else:
 #     print('есть дубли')
+
+""" сортировка словаря по значениям или ключам"""
+# a = {
+#     1: 25,
+#     2: 55,
+#     3: 77
+# }
+# b ={
+#     4: 12,
+#     5: 21,
+#     6: 99
+# }
+# c ={
+#     7: 1,
+#     8: 49,
+#     9: 72
+# }
+#
+# lst = [a,b,c]
+# D = {}
+#
+# for i in lst:
+#     D.update(i)
+#
+# # result_dict = sorted(D.values())# по значениям
+# result_dict = sorted(D,key=D.get)
+#
+# print(D)
+# print(result_dict)
+# new_D={}   # по ключам
+# for key in result_dict:
+#     new_D[key] = D[key]
+#
+# print(new_D)
+
+"""определить самую длинную и короткую строку
+/ удалить пустые строки"""
+
+# stroka = "String will never be empty and you do          not need to a account for different data types."
+# slova = stroka.split()
+# print(slova)
+# long_min = 999
+# long_max = 0
+# for i in slova:
+#     if len(i) < long_min:
+#         long_min = len(i)
+#         short = i
+#     if len(i) > long_max:
+#         long_max = len(i)
+#         long = i
+# print(short)
+# print(long)
+
+'''найти наичаще встречающиеся слова /
+ сколько раз каждое слово встречается в строке'''
+
+# stroka = "String will never be empty and you do do do do a a a  a       a a a not need to a account for different data types."
+# slova = stroka.split()
+# kolvo =0
+# dict = {}
+# for i in slova:
+#     kolvo = slova.count(i)
+#     dict[i] = kolvo
+# print(dict)
+
+"""на примере Банк карты спрятать всё
+ кроме последних 4-х символов"""
+
+# string = '5465 7788 2131 6577'
+# new = ''
+# for i in range(len(string) - 4):
+#     if string[i] != ' ':
+#         new += '*'
+#     else:
+#         new += ' '
+# new += string[-4:]
+# print(new)
+
